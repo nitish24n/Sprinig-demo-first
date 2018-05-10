4e6a16a595ee6fe790865be41407a983cda928d4
nitish24n <nitish24m@gmail.com>
2018-05-10 22:50:39 +0530

    setter injection - part 2

---
+++ com/firstPkg/springDemo/SetterDemoApp.java
@@ -0,0 +1,24 @@
+package com.firstPkg.springDemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//Retrieve bean from spring container
+		CricketCoach theCoach = context.getBean("myCricketCoach",CricketCoach.class);
+		
+		//call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		
+		//clean the context
+		context.close();
+	}
+
+}
